66c437485310505343194552d81b69278da51984
therealhesham <heshammoha231992@gmail.com>
2023-09-08 15:14:06 +0300

    commit on master

--- modules/transactionroute.js
+++ modules/transactionroute.js
@@ -33,22 +33,24 @@ res.send(delet)
     
     
     })
-appTransactionRoute.get("/firsttansactionlist",(req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
-    res.header({"Access-Control-Allow-Credentials": true});
-    res.header("Access-Control-Max-Age", 24*60*60*1000);
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+appTransactionRoute.get("/firsttansactionlist",
+// (req,res,next)=>{
+//     res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
+//     res.header({"Access-Control-Allow-Credentials": true});
+//     res.header("Access-Control-Max-Age", 24*60*60*1000);
+//       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
 
-    const sender = req.cookies.token
-  // console.log(sender)
-  if(!sender) return res.send("not authenticated");
-  const decoder =  jwt.verify(sender,process.env.MYSECRET)
+//     const sender = req.cookies.token
+//   // console.log(sender)
+//   if(!sender) return res.send("not authenticated");
+//   const decoder =  jwt.verify(sender,process.env.MYSECRET)
   
-if(!decoder) return res.send("not authenticated");
-next()}
+// if(!decoder) return res.send("not authenticated");
+// next()}
 
-,async(req,res)=>{
+// ,
+async(req,res)=>{
     
     
     try {
@@ -64,9 +66,9 @@ next()}
         
         // const sender = cookies.token
 
-        const sender = req.cookies.token
+        // const sender = req.cookies.token
         
-        const decoder = jwt.verify(sender,process.env.MYSECRET)
+        // const decoder = jwt.verify(sender,process.env.MYSECRET)
 
         const finder = await modelexport.find()
         res.send(finder)
